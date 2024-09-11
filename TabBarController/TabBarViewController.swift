76b0b203c9ef9e03e135cbf6b4d312cc57fb549c
slenbder <marjassler@ya.ru>
2024-09-11 10:28:46 +0300

    Переделан TabBarController. Конфигурация контроллера вынесена из жизненного цикла в отдельную функцию

--- TabBarController/TabBarViewController.swift
+++ TabBarController/TabBarViewController.swift
@@ -7,41 +7,41 @@
 
 import UIKit
 
-final class TabBarViewController: UITabBarController {
+class TabBarController: UITabBarController {
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        let trackersViewController = TrackerViewController()
-        let trackersNavigationController = UINavigationController(rootViewController: trackersViewController)
-        trackersNavigationController.tabBarItem = UITabBarItem(
-            title: "Трекеры",
-            image: UIImage(named: "tracker_active"),
-            selectedImage: nil
-        )
-        
-        let statsViewController = StatsViewController()
-        statsViewController.tabBarItem = UITabBarItem(
-            title: "Статистика",
-            image: UIImage(named: "statistics_active"),
-            selectedImage: nil
-        )
-        
-        self.viewControllers = [trackersNavigationController, statsViewController]
-        
-        addTabBarBorder()
+      super.viewDidLoad()
+      configureTabBar()
+      tabBarBorder()
     }
     
-    private func addTabBarBorder() {
-        let border = UIView()
-        border.backgroundColor = .lightGray
-        border.translatesAutoresizingMaskIntoConstraints = false
-        tabBar.addSubview(border)
-        
-        NSLayoutConstraint.activate([
-            border.topAnchor.constraint(equalTo: tabBar.topAnchor),
-            border.leadingAnchor.constraint(equalTo: tabBar.leadingAnchor),
-            border.trailingAnchor.constraint(equalTo: tabBar.trailingAnchor),
-            border.heightAnchor.constraint(equalToConstant: 1)
-        ])
+    private func configureTabBar() {
+      let trackerViewController = TrackerViewController()
+      let statisticsViewController = StatisticsViewController()
+
+      trackerViewController.title = "Трекеры"
+      statisticsViewController.title = "Статистика"
+
+      trackerViewController.tabBarItem.image = UIImage(named: "trackers_active")
+      statisticsViewController.tabBarItem.image = UIImage(named: "statistics_active")
+
+      let trackerNavigationController = UINavigationController(rootViewController: trackerViewController)
+      trackerNavigationController.navigationBar.prefersLargeTitles = true
+      let statisticsNavigationController = UINavigationController(rootViewController: statisticsViewController)
+      statisticsNavigationController.navigationBar.prefersLargeTitles = true
+
+      setViewControllers([trackerNavigationController, statisticsNavigationController], animated: true)
+    }
+    
+    private func tabBarBorder() {
+      let border = UIView()
+      border.backgroundColor = .lightGray
+      border.translatesAutoresizingMaskIntoConstraints = false
+      tabBar.addSubview(border)
+
+        border.topAnchor.constraint(equalTo: tabBar.topAnchor).isActive = true
+        border.leadingAnchor.constraint(equalTo: tabBar.leadingAnchor).isActive = true
+        border.trailingAnchor.constraint(equalTo: tabBar.trailingAnchor).isActive = true
+        border.heightAnchor.constraint(equalToConstant: 1).isActive = true
     }
 }
