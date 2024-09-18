47bfd24980ce1dfbd5a182c80b33a8499b6ded28
slenbder <marjassler@ya.ru>
2024-09-18 16:05:41 +0300

    Реализовал Онбоардинг экраны

---
+++ Tracker/CategoryViewController/CategoryViewModel.swift
@@ -0,0 +1,7 @@
+//
+//  CategoryViewModel.swift
+//  Tracker
+//
+//  Created by Кирилл Марьясов on 18.09.2024.
+//
+
---
+++ Tracker/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,85 @@
+//
+//  Untitled 2.swift
+//  Tracker
+//
+//  Created by Кирилл Марьясов on 17.09.2024.
+//
+
+import Foundation
+import UIKit
+
+final class OnboardingViewController: UIViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+  
+  private let pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+  private let pageControl = UIPageControl()
+  private var pages = [UIViewController]()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setupPages()
+    setupPageViewController()
+    setupPageControl()
+  }
+  
+  private func setupPages() {
+    let page1 = OnboardingPageViewController(imageName: "onboardingBlue", text: "Отслеживайте только то, что хотите")
+    let page2 = OnboardingPageViewController(imageName: "onboardingRed", text: "Даже если это не литры воды и йога")
+    pages = [page1, page2]
+  }
+  
+  private func setupPageViewController() {
+    pageViewController.dataSource = self
+    pageViewController.delegate = self
+    addChild(pageViewController)
+    view.addSubview(pageViewController.view)
+    pageViewController.didMove(toParent: self)
+    
+    pageViewController.setViewControllers([pages[0]], direction: .forward, animated: true, completion: nil)
+    pageViewController.view.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      pageViewController.view.topAnchor.constraint(equalTo: view.topAnchor),
+      pageViewController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      pageViewController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+      pageViewController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+    ])
+  }
+  
+  private func setupPageControl() {
+    pageControl.numberOfPages = pages.count
+    pageControl.currentPage = 0
+    pageControl.pageIndicatorTintColor = UIColor.lightGray
+    pageControl.currentPageIndicatorTintColor = UIColor.black
+    pageControl.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(pageControl)
+    
+    NSLayoutConstraint.activate([
+      pageControl.widthAnchor.constraint(equalTo: view.widthAnchor),
+      pageControl.heightAnchor.constraint(equalToConstant: 18),
+      pageControl.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -168)
+    ])
+  }
+  
+  // MARK: - UIPageViewControllerDataSource
+  
+  func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+    guard let currentIndex = pages.firstIndex(of: viewController), currentIndex > 0 else {
+      return nil
+    }
+    return pages[currentIndex - 1]
+  }
+  
+  func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+    guard let currentIndex = pages.firstIndex(of: viewController), currentIndex < pages.count - 1 else {
+      return nil
+    }
+    return pages[currentIndex + 1]
+  }
+  
+  // MARK: - UIPageViewControllerDelegate
+  
+  func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+    if completed, let currentViewController = pageViewController.viewControllers?.first, let currentIndex = pages.firstIndex(of: currentViewController) {
+      pageControl.currentPage = currentIndex
+    }
+  }
+}
