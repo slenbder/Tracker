67b5be920ee90f0034c2f51b411ebb06f2f95d53
slenbder <marjassler@ya.ru>
2024-09-24 19:55:10 +0300

    Исправлены локали на экранах онбординга

--- Tracker/Onboarding/OnboardingViewController.swift
+++ Tracker/Onboarding/OnboardingViewController.swift
@@ -29,9 +29,9 @@ final class OnboardingViewController: UIPageViewController, UIPageViewController
     // MARK: - Private Methods
     
     private func setupPages() {
-        let page1 = OnboardingPageViewController(imageName: "onboardingBlue", text: "Отслеживайте только то, что хотите")
-        let page2 = OnboardingPageViewController(imageName: "onboardingRed", text: "Даже если это не литры воды и йога")
-        pages = [page1, page2]
+      let page1 = OnboardingPageViewController(imageName: "onboardingBlue", text: localizedString(key:"onboardScreen1"))
+      let page2 = OnboardingPageViewController(imageName: "onboardingRed", text: localizedString(key:"onboardScreen2"))
+      pages = [page1, page2]
     }
     
     private func setInitialViewController() {
